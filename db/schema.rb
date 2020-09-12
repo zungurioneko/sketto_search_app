80cd30306acc7d4c400799b5c11c3bc57e387e11
zungurioneko <zungurioneko@gmail.com>
2020-09-12 17:58:59 +0900

    before_actionでログインを必須化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_123932) do
+ActiveRecord::Schema.define(version: 2020_09_12_050907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2020_09_10_123932) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "message"
+    t.string "baseball_carrer"
+    t.string "position"
+    t.string "introduce"
+    t.string "contact"
     t.index ["email"], name: "index_players_on_email", unique: true
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
@@ -35,6 +41,12 @@ ActiveRecord::Schema.define(version: 2020_09_10_123932) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "message"
+    t.string "introduce"
+    t.string "history"
+    t.string "style"
+    t.string "contact"
     t.index ["email"], name: "index_teams_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teams_on_reset_password_token", unique: true
   end
