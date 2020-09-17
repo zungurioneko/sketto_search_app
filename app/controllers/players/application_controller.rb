8acd34c14d9af5337c2fb6510e0696f4f14f8a49
zungurioneko <zungurioneko@gmail.com>
2020-09-17 21:59:28 +0900

    fontawesomeのアイコンに新規登録へのリンクを作成

--- app/controllers/players/application_controller.rb
+++ app/controllers/players/application_controller.rb
@@ -1,4 +1,7 @@
 class Players::ApplicationController < ApplicationController
   layout "layouts/application"
   before_action :authenticate_player!
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "top#index"
   devise_for :players
   devise_for :teams
+  get "players/:id" => "players#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
